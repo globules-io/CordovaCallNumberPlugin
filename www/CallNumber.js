e07ccf3a604c06cf2ba0fe7ddb746450bdf6ddef
Eric <info@ericparlier.com>
2020-08-28 12:24:22 -0400

    plugin id and JS names

--- www/CallNumber.js
+++ www/CallNumber.js
@@ -1,20 +1,18 @@
 var CallNumber = function(){};
 
-CallNumber.prototype.callNumber = function(success, failure, number, bypassAppChooser){
+CallNumber.prototype.dial = function(success, failure, number, bypassAppChooser){
     cordova.exec(success, failure, "CallNumber", "callNumber", [number, bypassAppChooser]);
 };
 
-CallNumber.prototype.isCallSupported = function(success, failure){
+CallNumber.prototype.isSupported = function(success, failure){
     cordova.exec(success, failure, "CallNumber", "isCallSupported");
 }
 
 //Plug in to Cordova
 cordova.addConstructor(function() {
-
     if (!window.Cordova) {
         window.Cordova = cordova;
     };
-
     if(!window.plugins) window.plugins = {};
-    window.plugins.CallNumber = new CallNumber();
+    window.plugins.phone = new CallNumber();
 });
